fd2cb7f83950d94bed742e8527e2baa8b3049fd5
Sai Manoj <saimanoj.napa@gmail.com>
2020-04-08 19:31:16 +0530

    first commit for assignment

---
+++ src/main/java/com/sapient/week1/Application.java
@@ -0,0 +1,15 @@
+package com.sapient.week1;
+
+public class Application {
+    
+    public Application() {
+        
+    }
+    
+    public int checkEven(int num) {
+       if(num % 2 ==0 )
+       return 1;
+       else 
+       return 0;
+    }
+}
