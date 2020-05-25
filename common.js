6f18650299aa5bcc8577452c2f7169623c0cd447
NAPA SAI MANOJ <45784287+saimanojsuper@users.noreply.github.com>
2020-05-25 09:05:43 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,5 @@
+var name =document.getElementById("name").nodeValue;
+var password =document.getElementById("password").nodeValue;
+
+document.getElementById("name1").innerHTML =name;
+document.getElementById("pass1").innerHTML = password;
